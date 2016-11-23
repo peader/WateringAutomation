7c3cf1d952c9cfdeeb217bb72d966abc4fbff7fe
PeterM <peter.mahady1@gmail.com>
2016-11-23 21:21:42 +0100

    add the first test to for the database routines

--- WateringSystem/Src/DbConnection/Models.py
+++ WateringSystem/Src/DbConnection/Models.py
@@ -8,13 +8,14 @@ Created on 14Nov.,2016
 from peewee import *
 from Src.Settings.Config import *
 
+
 mysql_db = MySQLDatabase(databaseName,host=hostName,port=portNumber,user=userName, passwd=password)
 
 class WateringLog(Model):
       # These are all the fields it has
     # match up CharField/IntegerField/etc with correct type
-    dbn = CharField(primary_key=True) # primary key = unique id
-    date = DateTimeField()
+    #dbn = CharField(primary_key=True) # primary key = unique id
+    date = DateField()
     time = TimeField()
     volWater = DoubleField()
     #grade_span_max = IntegerField()
@@ -23,5 +24,29 @@ class WateringLog(Model):
     class Meta:
         database = mysql_db
         db_table = 'WateringLog'
+        
+    def logWateringInfo(self, todaysDate, currentTime, waterVolume):
+        try:
+            with mysql_db.transaction():
+                 WateringLog.create(date=todaysDate, time=currentTime, volWater=waterVolume)
+            print('watering information successfully saved to database')
+        except IntegrityError:
+            print('failed to save watering information to database')
+        
+        
+class MoistureContentLog(Model):
+      # These are all the fields it has
+    # match up CharField/IntegerField/etc with correct type
+    dbn = CharField(primary_key=True) # primary key = unique id
+    date = DateField()
+    time = TimeField()
+    moistureContent = DoubleField()
+    #grade_span_max = IntegerField()
+    #total_students = IntegerField()
+    
+    class Meta:
+        database = mysql_db
+        db_table = 'MoistureContentLog'
+
 
    
--- WateringSystem/Src/Tests/Tests.py
+++ WateringSystem/Src/Tests/Tests.py
@@ -6,17 +6,20 @@ Created on 15Nov.,2016
 import unittest
 from Src.DbConnection import Models
 from Src.DbConnection.Models import WateringLog
+from datetime import datetime
 
+d = datetime.now()
+temp =  WateringLog()
 
 class Test(unittest.TestCase):
-
-
+    
     def setUp(self):
-        #WateringLog.create_table()
-        temp = WateringLog.create()
+        if not temp.table_exists():
+            temp.create_table()
+        #temp = WateringLog.create()
 
-    def testQuery1(self):
-        pass
+    def test_Query1(self):
+        temp.logWateringInfo(d.date() , d.time(), 200)
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.QueryTest1']
